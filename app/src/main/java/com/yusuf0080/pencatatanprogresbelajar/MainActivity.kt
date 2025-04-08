4269a155156c8cb0a2eb0d159a0d3853bbfa8458
yusufsuryaM <yusufsuryamulyawan@gmail.com>
2025-04-09 06:12:32 +0700

    Menambahkan MainActivity untuk menginisialisasi aplikasi dan navigasi

--- app/src/main/java/com/yusuf0080/pencatatanprogresbelajar/MainActivity.kt
+++ app/src/main/java/com/yusuf0080/pencatatanprogresbelajar/MainActivity.kt
@@ -1,4 +1,31 @@
 package com.yusuf0080.pencatatanprogresbelajar
 
-class MainActivity {
+import PencatatanProgresBelajarTheme
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.mutableStateListOf
+import androidx.navigation.compose.rememberNavController
+import com.yusuf0080.pencatatanprogresbelajar.data.model.StudyLog
+import com.yusuf0080.pencatatanprogresbelajar.navigation.SetupNavGraph
+import com.yusuf0080.pencatatanprogresbelajar.ui.theme.PencatatanProgresBelajarTheme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnrememberedMutableState")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val logs = mutableStateListOf<StudyLog>()
+            val navController = rememberNavController()
+
+            PencatatanProgresBelajarTheme {
+                SetupNavGraph(
+                    navController = navController,
+                    logs = logs,
+                    addLog = { logs.add(it) }
+                )
+            }
+        }
+    }
 }
