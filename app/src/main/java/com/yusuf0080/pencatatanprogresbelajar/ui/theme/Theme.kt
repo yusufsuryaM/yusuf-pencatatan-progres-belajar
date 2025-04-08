33e691aff374e0555ed78ba77b4d0299878694ae
yusufsuryaM <yusufsuryamulyawan@gmail.com>
2025-04-09 06:04:29 +0700

    Menambahkan UI theme components

--- app/src/main/java/com/yusuf0080/pencatatanprogresbelajar/ui/theme/Theme.kt
+++ app/src/main/java/com/yusuf0080/pencatatanprogresbelajar/ui/theme/Theme.kt
@@ -1,42 +1,43 @@
 package com.yusuf0080.pencatatanprogresbelajar.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+// Default Typography
+private val AppTypography = Typography()
+
+// Default Shapes
+private val AppShapes = Shapes()
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
+    secondary = PurpleGrey40,
+    onSecondary = Color.Black,
+    background = Color.White,
+    onBackground = Color.Black,
+    surface = Color(0xFFF5F5F5),
+    onSurface = Color.Black,
+)
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    onPrimary = Color.Black,
+    secondary = PurpleGrey80,
     onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    background = Color(0xFF121212),
+    onBackground = Color.White,
+    surface = Color(0xFF1E1E1E),
+    onSurface = Color.White,
 )
 
 @Composable
 fun PencatatanProgresBelajarTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -52,7 +53,8 @@ fun PencatatanProgresBelajarTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        typography = AppTypography,
+        shapes = AppShapes,
         content = content
     )
 }
