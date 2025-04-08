a5f1842f7297a3834de132c55da2fed1cfef233b
yusufsuryaM <yusufsuryamulyawan@gmail.com>
2025-04-09 06:06:04 +0700

    Menambahkan StatsViewModel

--- app/src/main/java/com/yusuf0080/pencatatanprogresbelajar/viewmodel/StatsViewModel.kt
+++ app/src/main/java/com/yusuf0080/pencatatanprogresbelajar/viewmodel/StatsViewModel.kt
@@ -1,4 +1,11 @@
 package com.yusuf0080.pencatatanprogresbelajar.viewmodel
 
-class StatsViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class StatsViewModel : ViewModel() {
+    var weeklyTarget by mutableIntStateOf(360)
+    var dailyTarget by mutableIntStateOf(60)
 }
